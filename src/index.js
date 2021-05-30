273d18b0a6c47c7d19f4772a3dcf175af66d34a8
frank <franktranvantu@gmail.com>
2021-05-30 23:02:51 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-const square = function(number) {
-  return number * number;
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const square = number => number * number;
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-console.log(square(2));
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
