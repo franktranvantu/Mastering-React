5422bcff52ce20fbde32a88e0ea31c18f9a14027
frank <franktranvantu@gmail.com>
2021-06-02 12:27:00 +0700

    Update

--- src/components/movies.js
+++ src/components/movies.js
@@ -6,29 +6,21 @@ class Movies extends Component {
     movies: getMovies()
   }
 
-  handleDelete = (id) => {
-    this.setState({
-      movies: this.state.movies.filter(movie => movie._id !== id)
-    });
-  }
-
   renderMovieBody() {
-    return this.state.movies.map(movie => (
+    return (
       <tbody>
-        <tr key={movie._id}>
-          <td>{movie.title}</td>
-          <td>{movie.genre.name}</td>
-          <td>{movie.numberInStock}</td>
-          <td>{movie.dailyRentalRate}</td>
-          <td><button onClick={() => this.handleDelete(movie._id)} className="btn btn-danger">Delete</button></td>
-        </tr>
+        {
+          this.state.movies.map(movie => (
+            <tr key={movie._id}>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+            </tr>
+          ))
+        }
       </tbody>
-    ));
-  }
-
-  renderMovieCount() {
-    const {length: count} = this.state.movies;
-    return count === 0 ? 'There are no movies.' : `Showing ${count} movies in the database.`;
+    );
   }
 
   renderMovieTable() {
@@ -50,10 +42,9 @@ class Movies extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        <p>{this.renderMovieCount()}</p>
-        {this.state.movies.length === 0 || this.renderMovieTable()}
-      </React.Fragment>
+      <div>
+        {this.renderMovieTable()}
+      </div>
     );
   }
 }
