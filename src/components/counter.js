8c37ab66275049ae1a716028f4fb4e9fe389137d
frank <franktranvantu@gmail.com>
2021-06-02 12:09:32 +0700

    Update

--- src/components/counter.js
+++ src/components/counter.js
@@ -2,24 +2,32 @@ import React, {Component} from 'react';
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ['tag1', 'tag2', 'tag3']
+    count: 0
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) {
-      return <p>There are no tags!</p>
-    }
-    return <ul>{this.state.tags.map((tag, index) => <li key={index}>{tag}</li>)}</ul>;
+  handleIncrement() {
+    console.log('Increment Clicked' + this.state.count);
   }
 
   render() {
     return (
       <div>
-        {this.state.tags.length === 0 && <p>Please create a new tag!</p>}
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    const {count} = this.state;
+    let classes = 'badge m-2 badge-';
+    classes += count === 0 ? 'warning' : 'primary';
+    return classes;
+  }
+
+  formatCount() {
+    const {count} = this.state;
+    return count === 0 ? 'Zero' : count;
   }
 }
 
