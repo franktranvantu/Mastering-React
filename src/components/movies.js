88674f4d86f4e12dfbcd0629b9a3f8e728c4b961
frank <franktranvantu@gmail.com>
2021-06-05 15:17:14 +0700

    Update

--- src/components/movies.js
+++ src/components/movies.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 import {getMovies} from '../services/fakeMovieService';
 import Like from "./common/like";
+import Pagination from "./common/pagination";
 
 class Movies extends Component {
   state = {
-    movies: getMovies()
+    movies: getMovies(),
+    pageSize: 4,
+    currentPage: 1
   }
 
   handleDelete = (id) => {
@@ -19,6 +22,10 @@ class Movies extends Component {
     movies[index] = {...movie};
     movies[index].liked = !movies[index].liked;
     this.setState({movies});
+  }
+
+  handlePageChange = (page) => {
+
   }
 
   render() {
@@ -59,6 +66,7 @@ class Movies extends Component {
           }
           </tbody>
         </table>
+        <Pagination itemsCount={count} pageSize={this.state.pageSize} onPageChange={this.handlePageChange}  />
       </div>
     );
   }
