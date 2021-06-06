1323019dec464448fbaa66a6c4db2b07d7b383a7
frank <franktranvantu@gmail.com>
2021-06-06 19:16:43 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as Sentry from "@sentry/react";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import Raven from 'raven-js';
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 
-Sentry.init({
-  dsn: "https://84c2888b49e54b5b90e7ce27fac9318e@o722716.ingest.sentry.io/5802753"
+Raven.config("https://84c2888b49e54b5b90e7ce27fac9318e@o722716.ingest.sentry.io/5802753", {
+  release: '1-0-0',
+  environment: 'development'
 });
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 import {toast} from 'react-toastify';
-import * as Sentry from "@sentry/react";
+import Raven from "raven-js";
 
 axios.interceptors.response.use(null, error => {
   const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
   if (!expectedError) {
-    Sentry.captureException(error);
+    Raven.captureException(error);
     toast.error('An unexpected error occurred.');
   }
 
