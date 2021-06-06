840367b95e934f11926fee64cddb0ee52b7c075c
frank <franktranvantu@gmail.com>
2021-06-06 14:11:03 +0700

    Update

--- src/components/common/form.js
+++ src/components/common/form.js
@@ -73,7 +73,7 @@ class Form extends Component {
 
   renderButton = (label) => {
     return (
-      <button disabled={this.validate()} className="btn btn-primary">Login</button>
+      <button disabled={this.validate()} className="btn btn-primary">{label}</button>
     );
   }
 }
