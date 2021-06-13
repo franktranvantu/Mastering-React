c630378afe062fc4de404628bf571e74fd56257d
frank <franktranvantu@gmail.com>
2021-06-13 23:11:49 +0700

    Update

--- src/components/common/protectedRoute.js
+++ src/components/common/protectedRoute.js
@@ -7,11 +7,12 @@ const ProtectedRoute = ({path, component: Component, render, ...rest}) => {
     <Route
       {...rest}
       render={props => {
-      if (!authService.getCurrentUser()) {
-        return <Redirect to="/login" />;
-      }
-      return Component ? <Component {...props} /> : render(props);
-    }} />
+        if (!authService.getCurrentUser()) {
+          return <Redirect to="/login" />;
+        }
+        return Component ? <Component {...props} /> : render(props);
+      }}
+    />
   );
 }
 
