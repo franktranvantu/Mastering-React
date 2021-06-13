17aa44c9560b83ef2343d236d39ed88498a63d76
frank <franktranvantu@gmail.com>
2021-06-14 00:26:55 +0700

    Update

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-# Mastering React
-1. JavaScript
-2. Components
-3. Tables
-4. Forms
-5. Routing
-6. HTTP Services
-7. Auth
-8. Deployment
+# Implementing a Higher Order Component
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Movie from "./hoc/Movie";
+
+class App extends Component {
+  render() {
+    return (
+      <Movie id={1} />
+    );
+  }
+}
+
+export default App;
